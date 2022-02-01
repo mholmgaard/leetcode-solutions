4dc62767f1339550c57d588c49cf014a5d8cb700
mikkelholmgaard <mikk7735@hotmail.com>
2022-02-01 19:07:38 +0100

    Java/twoSum/ no point in creating an empty array

--- Java/twoSum.java
+++ Java/twoSum.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[0];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    result = new int[] {i, j};
+                    return new int[] {i, j};
                 }
             }
         }
-        return result;
+        return null;
     }
 }
