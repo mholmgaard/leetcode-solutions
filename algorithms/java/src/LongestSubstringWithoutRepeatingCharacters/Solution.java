377ff1ddcf496d7aadf4028089296b785ff4cdd2
mikkelholmgaard <mikk7735@hotmail.com>
2022-02-03 01:09:16 +0100

    Use a HashSet instead of HashMap

--- algorithms/java/src/LongestSubstringWithoutRepeatingCharacters/Solution.java
+++ algorithms/java/src/LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -1,24 +1,24 @@
 package LongestSubstringWithoutRepeatingCharacters;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int length = 0;
         for (int i = 0; i < s.length(); i++) {
-            HashMap<Character, Integer> map = new HashMap<>();
+            HashSet<Character> set = new HashSet<>();
 
             for (int j = i; j < s.length(); j++) {
                 char c = s.charAt(j);
-                if (!map.containsKey(c)) {
-                    map.put(c, i);
+                if (!set.contains(c)) {
+                    set.add(c);
                 } else {
                     break;
                 }
             }
 
-            if (map.size() > length) {
-                length = map.size();
+            if (set.size() > length) {
+                length = set.size();
             }
         }
         return length;
