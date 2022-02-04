4496468558e3299761ccbcace320cc212eac197f
mikkelholmgaard <mikk7735@hotmail.com>
2022-02-04 21:01:12 +0100

    Added palindromeNumber

---
+++ algorithms/java/src/PalindromeNumber/Solution.java
@@ -0,0 +1,7 @@
+package PalindromeNumber;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        return (Integer.toString(x).equals(new StringBuilder(Integer.toString(x)).reverse().toString()));
+    }
+}
